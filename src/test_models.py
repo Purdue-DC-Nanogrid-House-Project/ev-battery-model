150494c6bf2c66739aebb6eedf6bf8841aa9e304
chubbsloki <loki.p.sriram@gmail.com>
2025-02-03 19:02:32 -0500

    Working on testing model with utility

--- src/test_models.py
+++ src/test_models.py
@@ -99,7 +99,96 @@ def test_ev_charging(ev_model, battery_model, initial_charge, target_charge):
     plt.tight_layout()  # Adjust layout to prevent overlap
     plt.show()
 
+def test_ev_charging_v2(ev_model,battery_model,home_model,utility_model,initial_charge,target_charge):
+        # Convert percentages to state of charge (SoC) in kWh for ev
+    initial_soc_ev = initial_charge * ev_model.x_bar_ev
+    target_soc_ev = target_charge * ev_model.x_bar_ev
 
+    # Convert percentage to state of charge (SoC) in Kwh for charger
+    initial_soc_b = 1.00 * battery_model.x_bar_b #assumes full charge
+
+    time_range = np.arange(0, 24, ev_model.dt)  # Time range for 24 hours
+
+    # Create DataFrame for EV
+    df_ev = pd.DataFrame({
+        'Time (hours)': time_range,
+        'SoC (kWh)': np.zeros(len(time_range)),  # Placeholder for SoC values
+        'EV_Battery Power (kW)': np.zeros(len(time_range))  # Placeholder for charger power values
+    })
+
+    # Create DataFrame for Battery
+    df_b = pd.DataFrame({
+        'Time (hours)': time_range,
+        'SoC (kWh)': np.zeros(len(time_range)),  # Placeholder for SoC values
+        'Charger_Battery Power (kW)': np.zeros(len(time_range))  # Placeholder for battery power values
+    })
+
+    df_utility = pd.DataFrame({
+        'Time (hours)': time_range,
+        'Utility Power (kW)': np.zeros(len(time_range))  # Placeholder for battery power values
+    })
+
+    #Set Initial states SOC's
+    df_ev.iloc[0,1] = initial_soc_ev
+    df_b.iloc[0,1] = initial_soc_b
+
+    print(f"Initial ev SOC: {df_ev.iloc[0,1] } kWh")
+    print(f"Initial b SOC: {df_b.iloc[0,1] } kWh")
+
+    #Set Initial Powers
+    df_ev.iloc[0,2] = 0
+    df_b.iloc[0,2] = 0
+    df_utility.iloc[0,1] = 0
+
+    for i in range(len(df_ev)-1):
+            # Check if the current time is greater than 2 hours
+            if df_ev.iloc[i,0] > 2:
+                # Check if the current SoC is less than the target SoC
+                if df_ev.iloc[i,1] < target_soc_ev:
+                    #Determining EV charge rate & Updating EV SOC
+                    U_EV = ev_model.p_c_bar_ev
+                    df_ev.iloc[i+1,1] = df_ev.iloc[i,1]*ev_model.sys_d.A + (ev_model.sys_d.B)*U_EV
+
+                    #Determining House Demand
+                    U_HOUSE = home_model.demand
+
+                    #Determining Total Demand
+                    U_TOTAL = U_HOUSE + U_EV
+
+                    #Solve for required battery discharge and utility requirements
+                    if df_b.iloc[i,1] != 0:
+                        if U_TOTAL >= battery_model.p_d_bar_b:
+                            P_discharge = battery_model.p_d_bar_b/battery_model.eta_d_b
+                            utility_model.utility = U_TOTAL - P_discharge
+                        else:
+                            P_discharge = U_TOTAL/battery_model.eta_d_b
+                            utility_model.utility = 0
+
+                    #Setting Battery SOC
+                    df_b.iloc[i+1,1] = df_b.iloc[i,1]*battery_model.sys_d.A - (battery_model.sys_d.B)*P_discharge
+
+                    #Setting Power
+                    df_ev.iloc[i+1,2] = ev_model.p_c_bar_ev * ev_model.eta_c_ev
+                    df_b.iloc[i+1,2] = -P_discharge* battery_model.eta_d_b
+                    df_utility.iloc[i+1,1] = -utility_model.utilty 
+                
+                else:
+                    df_ev.iloc[i+1, 1] =  df_ev.iloc[i, 1] #Battery Reached desired state
+                    df_b.iloc[i+1, 1] =  df_b.iloc[i, 1]
+
+                    df_ev.iloc[i+1, 2] = 0  # No charging power
+                    df_b.iloc[i+1, 2] = 0  # No battery power delivered
+                    df_utility.iloc[i+1,1] = 0 #No
+            else:
+                df_ev.iloc[i+1, 1] =  df_ev.iloc[i, 1] #Battery Reached desired state
+                df_b.iloc[i+1, 1] =  df_b.iloc[i, 1]
+
+
+                df_ev.iloc[i+1, 2] = 0  # No charging power
+                df_b.iloc[i+1, 2] = 0  # No battery power delivered
+
+
+     
 
 if __name__ == "__main__":
     # This block is not needed since we will call the function from main.py
